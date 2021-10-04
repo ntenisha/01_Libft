6a415fa16fa6eb2aa0825b0df6ccc55427f7db4a
ntenisha <91886143+ntenisha@users.noreply.github.com>
2021-10-04 15:22:40 +0300

    Update memset.c

--- memset.c
+++ memset.c
@@ -1,5 +1,15 @@
-void *ft_memset( void *memptr, int val, size_t num )
+void *ft_memset(void *memptr, int val, size_t num)
 {
-
+  unsigned char *ptr;
+  unsigned char simbol;
   
+  ptr = (unsigned char *)memptr;
+  simbol = (unsigned char)val;
+  
+  while (num > 0)
+  {
+	  *ptr = simbol;
+	  num--;
+	  *ptr++;
+  }
 }
