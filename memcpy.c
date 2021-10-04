0b2a027b710153de50dba3a22abeeeab3db5f04f
ntenisha <91886143+ntenisha@users.noreply.github.com>
2021-10-04 16:23:39 +0300

    Update memcpy.c

--- memcpy.c
+++ memcpy.c
@@ -1,5 +1,17 @@
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t len)
 {
-	unsigned char de;
-	unsigned char src;
+	unsigned char *de;
+	unsigned char *sr;
+	
+	de = (unsigned char *)dest;
+	sr = (unsigned char *)src;
+	
+	while (len > 0)
+	{
+		*de = *sr;
+		de++;
+		sr++;
+		len--;
+	}
+	return(dest);
 }
