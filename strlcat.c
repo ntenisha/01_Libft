b81d63f21e89acdf1e02c5711b92d84d0bb397f5
ntenisha <91886143+ntenisha@users.noreply.github.com>
2021-10-05 16:56:30 +0300

    Create strlcat.c

---
+++ strlcat.c
@@ -0,0 +1,26 @@
+
+size_t ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t  i;
+    size_t  j;
+    size_t  len_s;
+    size_t  len_d;
+    
+    len_s = ft_strlen(src);
+    len_d = ft_strlen(dst);
+    i = 0;
+    j = len_d;
+    if (len_d < size - 1 && size > 0)
+    {
+        while (src[i] && len_d + i < size - 1)
+        {
+            dst[j] = src [i];
+            j++;
+            i++;
+        }
+        dst[j] = '\0';
+    }
+    if (len_d >= size)
+        len_d = size;
+    return (len_d + len_s);
+}
