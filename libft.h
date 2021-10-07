ed7fdebce02f36faaa6d926d448a6d6bb3afabca
ntenisha <91886143+ntenisha@users.noreply.github.com>
2021-10-07 17:14:17 +0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -16,25 +16,28 @@
 # include <string.h>
 # include <stdlib.h>
 
+int		ft_atoi(char *str);
 void	ft_bzero(void *s, size_t num);
 int	    ft_isalnum(char c);
 int	    ft_isalpha(char c);
 int	    ft_isascii(int c);
 int	    ft_isdigit(char c);
 int	    ft_isprint(char c);
+int     ft_tolower(int c);
+int     ft_toupper(int c);
 void    *ft_memchr(const void *str, int ch, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t len);
 void    *ft_memmove(void* dest, const void *src, size_t count);
 void	*ft_memset(void *memptr, int val, size_t num);
 char    *ft_strchr(const char *str, int ch);
+char    *ft_strrchr(const char *str, int ch);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 size_t  ft_strlcpy (char *dst, const char *src, size_t size);
 size_t  ft_strlcat(char *dst, const char *src, size_t size);
 size_t  ft_strlen(const char *str);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-int     ft_strncmp(const char *s1, const char *s2, size_t n);
-char    *ft_strrchr(const char *str, int ch);
-int     ft_tolower(int c);
-int     ft_toupper(int c);
+int		ft_memcmp(const void *buf1, const void *buf2, size_t count)
+char	*ft_strdup(const char *src)
+void	*ft_calloc(size_t num, size_t size)
 
 #endif
