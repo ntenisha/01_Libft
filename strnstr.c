585c7999aa4893028f5c18837500a411fde3d001
ntenisha <91886143+ntenisha@users.noreply.github.com>
2021-10-11 15:10:33 +0300

    Update strnstr.c

--- strnstr.c
+++ strnstr.c
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (big[i] != '\0' && i <= len)
 	{
 		j = 0;
-		while (little[j] != '\0' && i + j <= len && big[i + j] == little[j])
+		while (little[j] != '\0' && i + j < len && big[i + j] == little[j])
 			j++;
 		if (little[j] == '\0')
 			return ((char *)big + i);
