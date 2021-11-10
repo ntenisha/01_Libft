cd0ccb6a834c40f5b64fa19fddfbf479c0c670b6
ntenisha <91886143+ntenisha@users.noreply.github.com>
2021-11-10 21:16:58 +0300

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,19 +6,11 @@
 /*   By: ntenisha <ntenisha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 20:59:41 by ntenisha          #+#    #+#             */
-/*   Updated: 2021/11/08 21:25:16 by ntenisha         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:55:39 by ntenisha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-
-#include <stdio.h>
-#include <unistd.h>
-
-
-
-
 
 static	int	ft_kol_slov(char const *s, char c)
 {
@@ -84,54 +76,12 @@ char	**ft_split(char const *s, char c)
 	char	**str;
 	int		num_w;
 
-	if (!s || !c)
-	{
-		str = (char **) malloc (sizeof (char *));
-		str[0] = NULL;
-		return (str);
-	}
+	if (!s)
+		return (NULL);
 	num_w = ft_kol_slov(s, c);
-printf("\n kol slov %d \n", num_w);
 	str = (char **) malloc (sizeof (char *) * (num_w + 1));
 	if (!str)
 		return (NULL);
 	str = ft_write_words(str, s, c);
 	return (str);
-}
-
-#include <stdio.h>
-#include <unistd.h>
-
-void	ft_print_array2(char **array)
-{
-	int		i;
-
-	i = 0;
-	while (array[i])
-	{
-		printf("%s \n" , array[i]);
-		i++;
-	}
-}
-
-int main(void)
-{
-	printf("checking split with a string containing only delimiters ");
-	char **splited;
-	splited = ft_split("    ", ' ');
-ft_print_array2(splited);
-	printf("\nchecking split with empty string ");
-	splited = ft_split("", ' ');
-ft_print_array2(splited);
-	printf("\nchecking split with string and \\0 as delimiter ");
-	splited = ft_split("Hello, world!", '\0');
-ft_print_array2(splited);
-	printf("\nchecking split with empty string and \\0 as delimiter ");
-	splited = ft_split("", '\0');
-ft_print_array2(splited);
-
-	printf("\n sei4as poyavitsya \n");
-	splited = ft_split("1112211", '2');
-ft_print_array2(splited);
-	return(0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ntenisha <ntenisha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 19:29:52 by ntenisha          #+#    #+#             */
-/*   Updated: 2021/10/26 22:11:09 by ntenisha         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:34:22 by ntenisha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,37 @@
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t num);
+void	*ft_calloc(size_t num, size_t size);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
+char	*ft_itoa(int n);
 void	*ft_memchr(const void *str, int ch, size_t len);
+int		ft_memcmp(const void *buf1, const void *buf2, size_t count);
 void	*ft_memcpy(void *dest, const void *src, size_t len);
 void	*ft_memmove(void *dest, const void *src, size_t count);
 void	*ft_memset(void *memptr, int val, size_t num);
-char	*ft_strchr(const char *str, int ch);
-char	*ft_strrchr(const char *str, int ch);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-size_t	ft_strlen(const char *str);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_memcmp(const void *buf1, const void *buf2, size_t count);
-char	*ft_strdup(const char *src);
-void	*ft_calloc(size_t num, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strcpy(char *dst, const char *src);
 void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strdup(const char *src);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *str, int ch);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
